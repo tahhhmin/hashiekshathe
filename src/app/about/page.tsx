e3de3879e1e207fb2be3e6d79b5e501f2a018939
tahhhmin <tajriantahmin@gmail.com>
2025-07-25 00:04:31 +0600

    about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,331 @@
 import React from 'react'
 import Styles from './page.module.css'
+import { Handshake, Mail, UserSearch, Zap } from 'lucide-react'
+import Button from '@/ui/button/Button'
 
 export default function page() {
     return (
         <section className='section'>
             <div className={Styles.container}>
-                <h1>About</h1>
+                <div className={Styles.leftColumn}>
+                    <div className={Styles.card}>
+                        <div className={Styles.cardHeader}>
+                            <div className={Styles.title}>
+                                <UserSearch className={Styles.icon} />
+                                <h2>About Us</h2>
+                            </div>
+                            <div className={Styles.subtitle}>
+                                <h3 className='muted-text'>Subtitle</h3>
+                            </div>
+                        </div>
+
+
+
+                        <div className={Styles.cardContent}>
+                            <p className={Styles.cardText}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+                            </p>
+                            <p className={Styles.cardText}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+                            </p>
+                        </div>
+
+                        <div className={Styles.cardfooter}>
+                            <Button
+                                variant='primary'
+                                label='Show More'
+                            />
+
+                            <Button
+                                variant='outlined'
+                                label='Show More'
+                            />
+                        </div>
+                    </div>
+
+                    <div className={Styles.card}>
+                        <div className={Styles.cardHeader}>
+                            <div className={Styles.title}>
+                                <UserSearch className={Styles.icon} />
+                                <h2>About Us</h2>
+                            </div>
+                            <div className={Styles.subtitle}>
+                                <h3 className='muted-text'>Subtitle</h3>
+                            </div>
+                        </div>
+
+
+
+                        <div className={Styles.cardContent}>
+                            <p className={Styles.cardText}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+                            </p>
+                            <p className={Styles.cardText}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+                            </p>
+                        </div>
+
+                        <div className={Styles.cardfooter}>
+                            <Button
+                                variant='primary'
+                                label='Show More'
+                            />
+
+                            <Button
+                                variant='outlined'
+                                label='Show More'
+                            />
+                        </div>
+                    </div>
+
+                    <div className={Styles.card}>
+                        <div className={Styles.cardHeader}>
+                            <div className={Styles.title}>
+                                <UserSearch className={Styles.icon} />
+                                <h2>About Us</h2>
+                            </div>
+                            <div className={Styles.subtitle}>
+                                <h3 className='muted-text'>Subtitle</h3>
+                            </div>
+                        </div>
+
+
+
+                        <div className={Styles.cardContent}>
+                            <p className={Styles.cardText}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+                            </p>
+                            <p className={Styles.cardText}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+                            </p>
+                        </div>
+
+                        <div className={Styles.cardfooter}>
+                            <Button
+                                variant='primary'
+                                label='Show More'
+                            />
+
+                            <Button
+                                variant='outlined'
+                                label='Show More'
+                            />
+                        </div>
+                    </div>
+
+                    <div className={Styles.card}>
+                        <div className={Styles.cardHeader}>
+                            <div className={Styles.title}>
+                                <UserSearch className={Styles.icon} />
+                                <h2>About Us</h2>
+                            </div>
+                            <div className={Styles.subtitle}>
+                                <h3 className='muted-text'>Subtitle</h3>
+                            </div>
+                        </div>
+
+
+
+                        <div className={Styles.cardContent}>
+                            <p className={Styles.cardText}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+                            </p>
+                            <p className={Styles.cardText}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua.
+                            </p>
+                        </div>
+
+                        <div className={Styles.cardfooter}>
+                            <Button
+                                variant='primary'
+                                label='Show More'
+                            />
+
+                            <Button
+                                variant='outlined'
+                                label='Show More'
+                            />
+                        </div>
+                    </div>
+                </div>
+                
+                
+                
+                <div className={Styles.rightColumn}>
+                    <div className={Styles.subCard}>
+                        <div className={Styles.subCardHeader}>
+                            <div className={Styles.title}>
+                                <Handshake className={Styles.icon} />
+                                <h2>About Us</h2>
+                            </div>
+                        </div>
+
+                        <div className={Styles.subCardContentButton}>
+                            <Button
+                                variant='primary'
+                                label='Join Us'
+                            />
+
+                            <Button
+                                variant='outlined'
+                                label='Join Us'
+                            />
+
+                            <Button
+                                variant='outlined'
+                                label='Join Us'
+                            />
+                        </div>
+                    </div>
+
+
+                    <div className={Styles.subCard}>
+                        <div className={Styles.subCardHeader}>
+                            <div className={Styles.title}>
+                                <Zap className={Styles.icon} />
+                                <h2>Our Impact</h2>
+                            </div>
+                        </div>
+
+                        <div className={Styles.subCardContentStats}>
+                            <div className={Styles.statContainer}>
+                                <h1>1,200+</h1>
+                                <p className='muted-text'>Volunteer Hours</p>
+                            </div>
+
+                            <div className={Styles.statContainer}>
+                                <h1>1,200+</h1>
+                                <p className='muted-text'>Volunteer Hours</p>
+                            </div>
+
+                            <div className={Styles.statContainer}>
+                                <h1>1,200+</h1>
+                                <p className='muted-text'>Volunteer Hours</p>
+                            </div>
+
+                            <div className={Styles.statContainer}>
+                                <h1>1,200+</h1>
+                                <p className='muted-text'>Volunteer Hours</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className={Styles.subCard}>
+                        <div className={Styles.subCardHeader}>
+                            <div className={Styles.title}>
+                                <Zap className={Styles.icon} />
+                                <h2>Follow Us</h2>
+                            </div>
+                        </div>
+
+                        <div className={Styles.subCardContentIconButton}>
+                            <Button
+                                variant='icon'
+                                icon='facebook'
+                                showIcon
+                            />
+
+                            <Button
+                                variant='icon'
+                                icon='twitter'
+                                showIcon
+                            />
+
+                            <Button
+                                variant='icon'
+                                icon='instagram'
+                                showIcon
+                            />
+
+                            <Button
+                                variant='icon'
+                                icon='linkedin'
+                                showIcon
+                            />
+                        </div>
+                    </div>
+
+
+                    <div className={Styles.subCard}>
+                        <div className={Styles.subCardHeader}>
+                            <div className={Styles.title}>
+                                <Mail className={Styles.icon} />
+                                <h2>Details</h2>
+                            </div>
+                        </div>
+
+                        <div className={Styles.subCardContentListItems}>
+                            <div className={Styles.listItem}>
+                                <h3 className='muted-text'>Founded</h3>
+                                <h3>2024</h3>
+                            </div>
+
+                            <div className={Styles.listItem}>
+                                <h3 className='muted-text'>Founded</h3>
+                                <h3>2024</h3>
+                            </div>
+
+                            <div className={Styles.listItem}>
+                                <h3 className='muted-text'>Founded</h3>
+                                <h3>2024</h3>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                </div>
             </div>
         </section>
     )
---
+++ src/app/donate/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from './page.module.css'
+
+export default function page() {
+    return (
+        <section className='section'>
+            <div className={Styles.container}>
+                <h1>Donate</h1>
+            </div>
+        </section>
+    )
+}
